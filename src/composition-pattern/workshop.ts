219285a2fc7b15405d85a011a21d66e4031b05cd
joonmoogo <junemuk10@gmail.com>
2023-07-20 16:40:31 +0900

    add : expand function

--- src/composition-pattern/workshop.ts
+++ src/composition-pattern/workshop.ts
@@ -8,19 +8,37 @@ abstract class Contact{
     protected contact : Contact[] = [];
 
     abstract sendEmail() : void;
-
+    abstract sendEmail(name:string) : void;
     add(contact : Contact) : void{
         this.contact.push(contact);
     }
 }
 
 class Person extends Contact{
-    name : string ='';
-    email : string ='';
-
+    private name : string;
+    private email : string;
+    
+    constructor(name:string,email:string){
+        super();
+        this.name = name;
+        this.email = email;
+    }
     sendEmail(): void {
         const post = new Postman();
         post.sendEmail(this.email);    
+    }
+    sendEmailTo(email:string){
+        const post = new Postman();
+        post.sendEmail(email);
+    }
+    getEmail() : string {
+        return this.email;
+    }
+    getName() : string{
+        return this.name;
+    }
+    getList() : Contact[]{
+        return this.contact;
     }
 }
 
@@ -35,17 +53,13 @@ class Group extends Contact{
 
 }
 
-// 연락처 생성
-const person1 = new Person();
-person1.name = 'John Doe';
-person1.email = 'john.doe@example.com';
+// Person 생성
+const person1 = new Person('John Doe',"johnDoe@naver.com");
 console.log(person1);
-
-const person2 = new Person();
-person2.name = 'Jane Smith';
-person2.email = 'jane.smith@example.com';
+const person2 = new Person('Jane Smith','Janesmith@naver.com');
 console.log(person2);
-// 그룹 생성
+
+// 전화번호부 그룹 생성
 const group1 = new Group();
 group1.name = 'Friends Group';
 group1.add(person1);
@@ -55,5 +69,12 @@ console.log(group1);
 console.log('-----');
 console.log(group1.sendEmail());
 
+// Person의 연락처에 다른 사람 연락처 추가
+person1.add(person2);
+person2.add(person1);
+console.log(person1.getList());
+console.log(person2.getList());
+console.log(person1);
+console.log(person2);
 
 
